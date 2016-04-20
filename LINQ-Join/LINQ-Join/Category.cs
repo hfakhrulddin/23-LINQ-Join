7927b3458bc13792e991862a93cde50ef88695a4
Hussen Fakhrulddin <hf556482@gmail.com>
2016-04-19 23:27:15 -0700

    init1

---
+++ LINQ-Join/LINQ-Join/Category.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQ_Join
+{
+    public class Category
+    {
+       public string Name { get; set; }
+       public int ID { get; set; }
+    }
+
+    public class Product
+    {
+        public string Name { get; set; }
+        public int CategoryID { get; set; }
+    }
+  
+}
--- LINQ-Join/LINQ-Join/Program.cs
+++ LINQ-Join/LINQ-Join/Program.cs
@@ -21,33 +21,32 @@ namespace LINQ_Join
 
             List<Person> people = new List<Person> { magnus, terry, charlotte };
             List<Pet> pets = new List<Pet> { barley, boots, whiskers, daisy };
-            jointer (people, pets);
+            jointer(people, pets);
+            jointer2(people, pets);
+
+            Category electronics = new Category { Name = "electronics", ID = 1 };
+            Category books = new Category { Name = "books", ID = 2 };
+            Category clothes = new Category { Name = "clothes", ID = 3 };
+            Category cars = new Category { Name = "Challenger", ID = 4 };
+
+            Product tv = new Product { Name = "tv", CategoryID = 1 };
+            Product pc = new Product { Name = "pc", CategoryID = 1 };
+            Product learnVB = new Product { Name = "learnVB", CategoryID = 2 };
+            Product shirt = new Product { Name = "shirt", CategoryID = 3 };
+
+            List<Category> categories = new List<Category> { electronics, books, clothes };
+            List<Product> products = new List<Product> { tv, pc, learnVB, shirt };
+            //jointer3(categories, products);
+            jointer4(categories, products);
+
             Console.ReadLine();
         }
 
-        //var innerGroupJoinQuery =
-        //                                              from category in categories // for all categories
-        //                                                                          // join with product table on product category ID
-        //                                              join prod in products
-        //                                              on category.ID equals prod.CategoryID into prodGroup
-        //                                              // create a new object with just the product and category names
-        //                                              select new { CategoryName = category.Name, Products = prodGroup };
-
-
-
-        //var leftOuterJoinQuery =
-        //                                            from category in categories
-        //                                            join prod in products
-        //                                            on category.ID equals prod.CategoryID into prodGroup
-        //                                            from item in prodGroup.DefaultIfEmpty(new Product { Name = String.Empty, CategoryID = 0 })
-        //                                            select new { CatName = category.Name, ProdName = item.Name };
-
-        // Create a list where each element is an anonymous type
-        // that contains the person's first name and a collection of 
-        // pets that are owned by them.
-
         public static void jointer(List<Person> people, List<Pet> pets)
         {
+            // Create a list where each element is an anonymous type
+            // that contains the person's first name and a collection of 
+            // pets that are owned by them.
             var query = from person in people
                         join pet in pets
                         on person equals pet.Owner
@@ -61,6 +60,71 @@ namespace LINQ_Join
                 foreach (var pet in owner.Pets)
                 { Console.WriteLine(pet.Name); }
             }
+        }
+
+        public static void jointer2(List<Person> people, List<Pet> pets)
+        {
+
+            // Create a list where each element is an anonymous 
+            // type that contains a person's name and 
+            // a collection of names of the pets they own.
+            var query2 =
+                            people.GroupJoin(pets,
+                            person => person,
+                            pet => pet.Owner,
+                            (person, petCollection) =>
+                                                        new
+                                                        {
+                                                            OwnerName = person.Name,
+                                                            Pets = petCollection.Select(pet => pet.Name)
+                                                        });
+            foreach (var owner in query2)
+            {
+                Console.WriteLine(owner.OwnerName);
+
+                foreach (var pet in owner.Pets)
+                { Console.WriteLine(pet); }
+            }
+        }
+
+        //public static void jointer3(List<Category> categories, List<Product> products)
+        //{
+        //    var innerGroupJoinQuery =
+        //                                from category in categories // for all categories
+        //                                join prod in products // join with product table on product category ID
+        //                                on category.ID equals prod.CategoryID into prodGroup // create a new object with just the product and category names
+        //                                select new { CategoryName = category.Name, Products = prodGroup };
+
+        //    foreach (var cate in innerGroupJoinQuery)
+        //    {
+        //        Console.WriteLine("*******");
+        //        Console.WriteLine(cate.CategoryName);
+        //        Console.WriteLine("-------");
+
+        //        foreach (var product in cate.Products)
+        //        { Console.WriteLine(product.Name); }
+        //    }
+        //}
+
+        public static void jointer4(List<Category> categories, List<Product> products)
+        {
+            var leftOuterJoinQuery =
+                                                        from category in categories
+                                                        join prod in products
+                                                        on category.ID equals prod.CategoryID into prodGroup
+                                                        from item in prodGroup.DefaultIfEmpty(new Product { Name = String.Empty, CategoryID = 0 })
+                                                        select new { CatName = category.Name, ProdName = item.Name, Products = prodGroup };
+
+            foreach (var cate in leftOuterJoinQuery)
+            {
+                Console.WriteLine("*******");
+                Console.WriteLine(cate.CatName);
+                Console.WriteLine("-------");
+
+                foreach (var product in cate.Products)
+                { Console.WriteLine(product.Name); }
+            }
+
         }
     }
 }
